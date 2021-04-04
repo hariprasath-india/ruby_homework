197407c0b66e18f318f16276ccaf0abd49aed2f9
Hari Prasath <hariven1998@gmail.com>
2021-04-04 16:38:38 +0530

    update

--- lib/homework.rb
+++ lib/homework.rb
@@ -4,5 +4,49 @@ require_relative "homework/version"
 
 module Homework
   class Error < StandardError; end
-  # Your code goes here...
+  
+  class Student
+    def initialize(params = {})
+      @name = params[:name] || ""
+      @attended = params[:attended] || "false"
+      @grade = params[:grade] || "O"
+    end
+
+    def to_s
+      "Name: #{@name} \nAttended: #{@attended} \nGrade: #{@grade}"
+    end
+
+    def name(params= {})
+      @name = params[:name] || @name
+    end
+
+    def attended(params= {})
+      @attended = params[:attended] || @attended
+    end
+
+    def grade(params= {})
+      @grade = params[:grade] || @grade
+    end
+
+
+  end
+
+  value =  Student.new(:name => "AAAAA")
+  puts (value)
+  puts ("value Changes")
+  value_name =  value.name(:name => "hariee")
+  puts (value)
+  value_attended =  value.attended(:attended => "True")
+  puts (value)
+  value_grade =  value.grade(:grade => "A")
+  puts (value)
+  class Classroom
+    def initialize
+      
+    end
+
+  end
+
+
+  
 end
